7bcb99678806502e6f26e73ad74f3e8be0a80213
Axis <erlia@naver.com>
2024-05-15 06:13:31 +0900

    11 22 33 4 5

--- src/Ex11_33.java
+++ src/Ex11_33.java
@@ -9,28 +9,30 @@ public class Ex11_33 {
         HashMap map=new HashMap();
 
         for(int i=0;i< data.length;i++){
-            if(map.containsKey(data[i])) {
-                Integer value = (Integer) map.get(data[i]);   //get(key값으로)->값반환
-                System.out.println( "value.intValue()->"+new Integer(value.intValue())+1);
-                map.put(data[i], new Integer(value.intValue())+1);  //깉은 키가 있으면 해당키의 값을 1을 더 증가시킨다.
+            if(map.containsKey(data[i])) {   //data의 자료가 맵의 키값으로 사용(알파벳이 키값으로 사용), 1.같은 키값이 있다면..
+                Integer value = (Integer) map.get(data[i]);   //get(key값으로)->값반환  2.그 키의 값의 숫자를 가져온다.
+               // System.out.println( "value.intValue()->"+new Integer(value.intValue())+1);
+                map.put(data[i], new Integer(value.intValue())+1);  //3.깉은 키가 있으면 해당키의 값을 1을 더 증가시킨다.
 
 
             }else {
-                map.put(data[i], new Integer(1));
+                map.put(data[i], new Integer(1));   //해당키가 처음이면 값으로 1로 처음으로 할당
 
             }
-        }
+        }  //결국 map에는 (F, 3)이런식으로 담긴다..F 키가 3개있다는 뜻
+
+
         Iterator it=map.entrySet().iterator();
 
         while (it.hasNext()){
-            Map.Entry e=(Map.Entry)it.next();
-            int value=((Integer)e.getValue()).intValue();
+            Map.Entry e=(Map.Entry)it.next();  //키와 값으로 이루어진 엔트리를 구하고
+            int value=((Integer)e.getValue()).intValue();  //그 엔트리에서 값(숫자)을 가져온다.
             System.out.println(e.getKey()+":"+printBar('#', value)+" "+value);
         }
 
     }
 
-    public  static String printBar(char ch, int value) {
+    public  static String printBar(char ch, int value) {  //value의 숫자만큼 #으로 표시하는 메서드
         char[] bar = new char[value];  //중복된 알파벳 갯수만큼의 크기로 배열을 만듦
 
         for (int i = 0; i < bar.length; i++)
