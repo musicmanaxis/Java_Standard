2a1b1ac4d4d30a886841177f9439e04c1ec3ad5b
Axis <erlia@naver.com>
2024-05-21 08:20:07 +0900

    11 22 33 4 5 6 77 7 88100 80 944 101 1234 2222

---
+++ src/Ex12/Ex12_3.java
@@ -0,0 +1,44 @@
+package Ex12;
+
+import java.util.ArrayList;
+
+class Fruit2{public String toString(){return "Fruit2";}}
+class Apple2 extends Fruit2{public String toString(){return "Apple2";}}
+class Grape2 extends Fruit2{public String toString(){return "Grape2";}}
+
+class Box2<T>{
+    ArrayList<T> list=new ArrayList<>();
+
+    void add(T item){list.add(item);}
+    ArrayList<T> getList(){return list;}
+    public String toString(){return list.toString();}
+
+}
+
+class FruitBox2<T> extends Box2<T>{ }
+
+class Juice{
+    String name="";
+    Juice(String name){
+        this.name=name+" Juice";
+    }
+    public String toString(){return name;}
+}
+
+class JuiceMaker{
+    String tmp="";
+    static Juice makeJuice(FruitBox2<?extends Fruit2> box){
+        for(FruitBox2 f:box.getList()) {
+            tmp+=""+f;
+            return new Juice(tmp);
+        }
+    }
+}
+
+public class Ex12_3 {
+    public static void main(String[] args) {
+
+        FruitBox2<Fruit> fruitBox2=new FruitBox2<>();
+        fruitBox2.add(new Fruit());
+    }
+}
