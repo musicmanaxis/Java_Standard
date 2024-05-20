08e1ccc052e6f9b6606eec289198d12aa9e26bd8
Axis <erlia@naver.com>
2024-05-21 05:23:20 +0900

    11 22 33 4 5 6 77 7 88100 80 9 10

--- src/Ex12/Ex12_1.java
+++ src/Ex12/Ex12_1.java
@@ -2,24 +2,25 @@ package Ex12;
 
 import java.util.ArrayList;
 
-class Fruit{
-	public String toString(){return "Fruit입니다.";}
-}
-class Apple extends Fruit{
-	public String toString(){return "Apple입니다";}
-}
-class Grape extends Fruit{
-	public String toString(){return "Grape입니다";}
-}
-class Toy{
-	public String toString(){return "Toy입니다";}
-}
+class Fruit{  	public String toString(){return "Fruit입니다.";}}
+class Apple extends Fruit{	public String toString(){return "Apple입니다";}}
+class Grape extends Fruit{	public String toString(){return "Grape입니다";}}
+class Toy{	public String toString(){return "Toy입니다";}}
 
+class Box<T>{   //제네릭은 타입을 지정하여 들어올수 있는 객체를 필터링하여 집어넣을수 있게 한다.  설계도
+	ArrayList<T> list=new ArrayList<T>();
+
+	void add(T item){list.add(item);}
+	T getList(int i){return list.get(i);}
+	int size(){return list.size();}
+	public String toString(){return list.toString();}
+	//list.toString() ->리트트의 내용을 문자열로 보여줌
+}
 
 public class Ex12_1{
 	public static void main(String[] args) {
-		Box<Fruit> fruitBox=new Box<Fruit>();
-		Box<Apple1> appleBox=new Box<>();
+		Box<Fruit> fruitBox=new Box<Fruit>();  //괴일만 들어올수 있게 객체를 만듬
+		Box<Apple1> appleBox=new Box<>();  //사과만 들어올수 있게..
 		Box<Grape> grapeBox=new Box<>();
 
 		fruitBox.add(new Apple());
@@ -31,18 +32,5 @@ public class Ex12_1{
 		System.out.println("과일박스:"+fruitBox);
 		System.out.println("사과박스:"+appleBox);
 		System.out.println("과일박스의 첫번재 아이템:"+fruitBox.getList(0));
-
-
-
 	}
-}
-
-class Box<T>{
-	ArrayList<T> list=new ArrayList<T>();
-
-	void add(T item){list.add(item);}
-	T getList(int i){return list.get(i);}
-	int size(){return list.size();}
-	public String toString(){return list.toString();}
-	//list.toString() ->리트트의 내용을 문자열로 보여줌
 }
--- src/Ex12/Ex12_2.java
+++ src/Ex12/Ex12_2.java
@@ -6,7 +6,7 @@ interface Eateble{}
 class Fruit1 implements Eateble{ public String toString(){ return "Fruit1";     }}
 class Apple1 extends Fruit1{    public String toString(){ return "Apple1";    }}
 class Grape1 extends Fruit1{    public String toString() {return "Grape1";    }}
-class Toy1{public String toString(){return "Toy1";}}
+class Toy1 implements Eateble{public String toString(){return "Toy1";}}  //여기를 이렇게 구현한다면...
 
 class FruitBox<T extends Fruit1 & Eateble> extends Box1<T>{}  //인터페이스와 클래스를 타입상속할 때 위와 같이 표현
 class Box1<T>{
@@ -23,17 +23,20 @@ public class Ex12_2 {
         FruitBox<Fruit1> fruitBox=new FruitBox<Fruit1>();
         FruitBox<Apple1> appleBox=new FruitBox<>();
         FruitBox<Grape1> grapeBox=new FruitBox<Grape1>();
+        Box1<Toy1> toy1Box=new Box1<>();
 
         fruitBox.add(new Fruit1());
         fruitBox.add(new Apple1());
         appleBox.add(new Apple1());
         // appleBox.add(new Grape1());
         grapeBox.add(new Grape1());
+        toy1Box.add(new Toy1());
 
 
         System.out.println(fruitBox);
         System.out.println(appleBox);
         System.out.println(fruitBox.getItem(0));
+        System.out.println(toy1Box);
 
     }
 }
