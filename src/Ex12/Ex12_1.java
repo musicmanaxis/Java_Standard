6de21a41b72373bc05c6fda67a144378a998b480
Axis <erlia@naver.com>
2024-05-21 05:45:43 +0900

    11 22 33 4 5 6 77 7 88100 80 944 10

--- src/Ex12/Ex12_1.java
+++ src/Ex12/Ex12_1.java
@@ -7,6 +7,9 @@ class Apple extends Fruit{	public String toString(){return "Apple입니다";}}
 class Grape extends Fruit{	public String toString(){return "Grape입니다";}}
 class Toy{	public String toString(){return "Toy입니다";}}
 
+//1.제네릭은 타입을 지정하여 2.실제 객체를 생성할 때 필터링을 할 목적으로 만들어진다.
+//Box<T>라고 설계도 즉 붕어빵 틀이라고 생각하고 제네릭클래스를 작성하자..
+
 class Box<T>{   //제네릭은 타입을 지정하여 들어올수 있는 객체를 필터링하여 집어넣을수 있게 한다.  설계도
 	ArrayList<T> list=new ArrayList<T>();
 
@@ -19,15 +22,17 @@ class Box<T>{   //제네릭은 타입을 지정하여 들어올수 있는 객체
 
 public class Ex12_1{
 	public static void main(String[] args) {
-		Box<Fruit> fruitBox=new Box<Fruit>();  //괴일만 들어올수 있게 객체를 만듬
-		Box<Apple1> appleBox=new Box<>();  //사과만 들어올수 있게..
+		//2.Box<T>설계도에 근거하여 실제 객체를 생성할때는 원하는 타입의 객체를 만들어 쓴다.
+
+		Box<Fruit> fruitBox=new Box<Fruit>();  //괴일과 그 상속한 객체만 들어올수 있게 객체를 만듬
+		Box<Apple> appleBox=new Box<>();  //사과만 들어올수 있게..
 		Box<Grape> grapeBox=new Box<>();
 
 		fruitBox.add(new Apple());
 		fruitBox.add(new Grape());
 
-		appleBox.add(new Apple1());
-		appleBox.add(new Apple1());
+		appleBox.add(new Apple());
+		appleBox.add(new Apple());
 
 		System.out.println("과일박스:"+fruitBox);
 		System.out.println("사과박스:"+appleBox);
