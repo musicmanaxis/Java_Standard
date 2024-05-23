e0cff883cc00b428bfda94268680774f3d143e46
Axis <erlia@naver.com>
2024-05-24 06:38:26 +0900

    11 22 33 454321 5 6 77 7 88100 80 944 101
    123124124
    1234
    2222

--- src/Ex6/Ex6_28.java
+++ src/Ex6/Ex6_28.java
@@ -2,19 +2,17 @@ package Ex6;
 
 public class Ex6_28 {
 
-	static int[] arr= new int[10];
-	
-	static {
-		for(int i=0 ; i<arr.length ; i++) {
-			arr[i]=(int)(Math.random()*10+1);
-		}
-	}
-	
-	public static void main(String[] args) {
-		for(int i=0 ; i<arr.length ;i++)
-			System.out.println("arr["+i+"]="+arr[i]);
-			
-			
-	}
+    static int[] arr=new int[10];
 
+    static {   //클래스 변수 초기화 블럭
+        for(int i=0; i<arr.length;i++){
+            arr[i]=(int)(Math.random()*10)+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i<arr.length;i++)
+            System.out.println("arr["+i+"]:"+arr[i]);
+
+    }
 }
