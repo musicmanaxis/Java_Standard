5c8a90b28fa4e627b8c28d8e812c3510f1f9e89c
Axis <erlia@naver.com>
2024-05-30 06:49:37 +0900

    11 22 33334 454321 5 6 77 7 88100 80 944 101
    123124124
    1234
    2222

---
+++ src/Ex13/Ex13_1.java
@@ -0,0 +1,48 @@
+package Ex13;
+
+//쓰레드를 구현하는 방식은 1.Thread클래스를 상속받아서 객체를 만들어 사용하거나
+//2.Runaable 인터페이스를 받아서 쓰는 방법이 있는데 주로 2번방법이 많이 쓰인다.
+//1.상속받아서 쓰면 그자체가 쓰레드 이기 때문에 단순히 객체를 생성해서 실행하면 되고
+//2.인터페이스를 쓰게 되면 반환되는 인터페이스 타입을 다시 쓰레드 객체의 인자로 집어넣어서 사용한다.
+
+public class Ex13_1 {
+
+    public static void main(String[] args) {
+        Thread_1 tr1=new Thread_1();
+        Thread_1 tr2=new Thread_1();
+
+        Thread ra=new Thread(new Thread_2());
+//        Runnable ra=new Thread_2();  아래 2줄을 위의 한줄로 표현
+//        Thread tr=new Thread(ra);
+
+        tr1.start();
+        tr2.start();
+
+        //start()의 역할은 main()와 같은 역할인 run()를 호출하여 새로운 스택을 생성하여 main()을 역할을 run()이 하게 된다.
+
+        ra.start();
+
+        //OS의 스케줄러에 따라 어떤것이 먼저 실행될지는 각기 다르다.
+
+    }
+}
+
+class Thread_1 extends Thread{
+    public void  run(){
+        for(int i=0 ;i<10 ;i++) {
+            //  System.out.print(this.getName());
+            System.out.println("--t1:"+getName()+"##");
+        }
+
+    }
+}
+
+class Thread_2 implements Runnable{
+    public void run(){
+        for(int i=0 ;i<10 ;i++) {
+            //  System.out.print(Thread.currentThread().getName());
+            System.out.print("Runable Thread:"+i+"**");
+        }
+    }
+
+}
--- src/Ex7/Ex7_9.java
+++ src/Ex7/Ex7_9.java
@@ -1,14 +1,44 @@
 package Ex7;//매개변수의 다형성: 조상타입을 매개변수로 하면 자손타입들도 집어넣을 수 있다.
 //배열을 이용하여 배열에 객체를 담아 저장하고 꺼낸다..여기선 장바구니 개념으로 이용했다.
 
+class Product1{
+	int price;
+	Product1(){};
+	Product1(int price){
+		this.price=price;
+	}
+}
+
+class Tv2 extends Product1{
+	Tv2(){
+		super(100);
+	}
+	public String toString() {
+		return "Tv";
+	}
+}
+
+class Computer1 extends Product1{
+	Computer1(){
+		super(200);
+	}
+	public String toString() {
+		return "Computer";
+	}
+}
+
+class Audio1 extends Product1{
+	Audio1(){super(50);}
+	public String toString(){return "Audio";}
+}
+
 class Buyer1{
 	int money=1000;
 	int bonusPoint=0;
 	Product1[] cart=new Product1[10];  //장바구니   
-	int i=0;
+	int i=0;   //장바구니의 인덱스를 위한 것
 	
 		void buy(Product1 p){   //매개변수의 다형성..
-		
 			if(money < p.price) {
 				System.out.println("There is no money");
 				return;
@@ -18,7 +48,6 @@ class Buyer1{
 			System.out.println(p+"를 구매하셧습니다. ");		
 			money -= p.price;
 			bonusPoint += (int)(p.price/10.0) ;
-		
 		}
 	
 		void summary() {   // 자기가 산 물건 목록보기  
@@ -28,57 +57,21 @@ class Buyer1{
 			for(int i=0; i<cart.length ;i++) {
 				if(cart[i]==null) break;
 				sum += cart[i].price; //합계 내기  
-				item += cart[i]+", ";  //장바구니 목록보기..
-			
-		}
-		
-		System.out.println("총구매액은 "+sum+"입니다.");
-		System.out.println(item+"을 구매했습니다.");
+				item += (i==0 )? ""+cart[i] : ", "+cart[i];
+			}
+		System.out.println("cart[0]="+cart[0]);
+		System.out.println("총구매액은 "+sum+"이고, "+"포인트는 "+bonusPoint+"입니다.");
+		System.out.println("구매목록은 "+item+"을 구매했습니다.");
 	}
-
-}
-
-
-class Product1{
-	int price;
-	
-	Product1(int price){
-		this.price=price;
-	}
-}
-
-
-class Tv2 extends Product1{
-	
-	Tv2(){
-		super(100);
-	}
-	
-	public String toString() {
-		return "Tv";
-	}
-	
-}
-
-class Computer1 extends Product1{
-	
-	Computer1(){
-		super(300);
-	}
-	
-	public String toString() {
-		return "Computer";
-	}
-		
 }
 
 public class Ex7_9 {
-
 	public static void main(String[] args) {
 		
 		Buyer1 b1=new Buyer1();
 		b1.buy(new Computer1());
 		b1.buy(new Tv2());
+		b1.buy(new Audio1());
 		b1.summary();
 	}
 
