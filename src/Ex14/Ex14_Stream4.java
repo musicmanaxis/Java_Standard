3b7c5d12c66ff5b241a82410851126b8c015b5e1
Erlia <erlia@naver.com>
2024-06-24 20:06:45 +0900

    12123 1 2 3 4 5

---
+++ src/Ex14/Ex14_Stream4.java
@@ -0,0 +1,38 @@
+package Ex14;
+
+
+import java.io.File;
+import java.util.stream.Stream;
+
+//스트림으 중간연산
+//map()->스트림의 요소변환, 예)파일스트림에서 스트링 스트림으로 변환
+// //peak()는 중간연산->forEach(), flatMap()->map이랑 비슷
+public class Ex14_Stream4 {
+
+    public static void main(String[] args) {
+        File[] files={new File("Ex1.java"), new File("Ex1.bak"), new File("Ex1"),
+                new File("Ex2.java"), new File("Ex1.txt")};
+
+        Stream<File> fileStream=Stream.of(files);
+
+        //Stream<String> fileNameStream=fileStream.map(File::getName);
+        Stream<String> fileNameStream=fileStream.map((f)->f.getName());
+        //객체의 메서드이므로 f.getName()으로 표현
+
+        fileNameStream.forEach(System.out::println);
+
+        fileStream = Stream.of(files);  //스트림을 다시 생성
+        System.out.println("중간연산 작업========");
+        fileStream.map(File::getName)
+                .filter(s->s.indexOf('.')!=-1)  //확장자가 없는 것은 제외
+                .map(s->s.substring(s.indexOf('.')+1))//확장자만 추출
+                .map(String::toUpperCase)
+                .distinct()
+                .forEach(System.out::println);
+
+        //11:00
+
+    }
+
+
+}
