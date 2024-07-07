48b97c2f81c3db610fce12920e24da846b939966
Axis <erlia@naver.com>
2024-07-08 00:40:08 +0900

    1 2 3 4 5

--- src/Ex9/Ex9_1.java
+++ src/Ex9/Ex9_1.java
@@ -1,8 +1,10 @@
 package Ex9;
 //Object 클래스의 equals()를 오버라이딩하여 같은값임을 확인하는 클래스
-//equals()는 원래 주소값을 변경하는 것이라 이것을 그대로 쓰면 각 객체 주소값이 달라 false를 반환한다.
-//이에 오버라이딩하여 객체가 가지고 있는 변수값이 서로 같다면 동일한 객체로 판단하게 작업한것이다.
+//equals()는 원래 주소값을 사용하는 것이라 이것을 그대로 쓰면 각 객체 주소값이 달라 false를 반환한다.
+//객체가 가지고 있는 변수값을 오버라이딩하여 객체가 가지고 있는 변수값이 서로 같다면 동일한 객체로 판단하게 작업한것이다.
 //대부분 이 메소드는 인스턴스 변수를 비교하게 오버라이딩하여 쓴다...
+//작설자가 직접 작성한 클래스나 StringBuffer 클래스는 객체가 동일한건지 비교하고 싶다면 equals()를 오버라이딩하여 사용
+//나머지 클래스들은 오버라이딩 되어 있음.
 
 
 class Ex9_1 {
@@ -23,6 +25,7 @@ class Ex9_1 {
 
 
 class Value{
+	
 	int value;
 	
 	Value(int value){
@@ -30,7 +33,7 @@ class Value{
 	}
 	
 	public boolean equals(Object obj) {   //오버라이딩 했다.
-		
+		//객체의 변수를 이용하여 오버라이딩 하면된다.
 		if(!(obj instanceof Value)) return false;  //Object가 Value 객체인지 사전검사  
 		
 		Value v=(Value)obj;    //실제 Object는 value변수가 없으므로 이를 형변환 한다.
