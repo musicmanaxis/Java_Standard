d5c54c12e4fb62eccdeff0e4eb250ff0c733f3ef
Erlia <erlia@naver.com>
2024-08-08 23:01:04 +0900

    12123 1 2 3 4 5 6 7 8 9 10

--- src/Ex14/Ex14_9.java
+++ src/Ex14/Ex14_9.java
@@ -10,23 +10,32 @@ import java.util.stream.Stream;
 public class Ex14_9 {
 
     public static void main(String[] args) {
-        File[] files={new File("Ex1.java"), new File("Ex1.bak"), new File("Ex1"),
-                new File("Ex2.java"), new File("Ex1.txt")};
+        File[] files={  new File("Ex1.java"), new File("Ex1.bak"), new File("Ex1"),
+                        new File("Ex2.java"), new File("Ex1.txt")
+                    };
+
+
+        for(int i=0; i< files.length; i++){
+            System.out.print(files[i].getName()+", ");
+        }
+
+        System.out.println("======");
 
         Stream<File> fileStream=Stream.of(files);
 
         //Stream<String> fileNameStream=fileStream.map(File::getName);
         Stream<String> fileNameStream=fileStream.map((f)->f.getName());
         //객체의 메서드이므로 f.getName()으로 표현
+        //map()으로 Stream<File>을 Stream<String>로 변환하는 것임.
 
         fileNameStream.forEach(System.out::println);
 
         fileStream = Stream.of(files);  //스트림을 다시 생성
         System.out.println("중간연산 작업========");
         fileStream.map(File::getName)
-                .filter(s->s.indexOf('.')!=-1)  //확장자가 없는 것은 제외
+                .filter(s->s.indexOf('.')!=-1)  //확장자가 없는 것은 제외  indexOf('.') 지정한 문자가 문자열에 몇번째에 있는지를 인덱스 위치(int)를 리턴한다.
                 .peek(s->System.out.printf("fileName=%s%n", s))  //peek:중간에 작업이 잘 되는지 확인용도로 쓰임
-                .map(s->s.substring(s.indexOf('.')+1))//확장자만 추출
+                .map(s->s.substring(s.indexOf('.')+1))//확장자만 추출, substring(숫자)startIndex부터 끝까지의 문자열을 리턴
                 .peek(s->System.out.printf("exetension=%s%n", s))
                 .map(String::toUpperCase)
                 .distinct()
