e3221375ca377bcc9736969b24954629900a49d4
Erlia <erlia@naver.com>
2024-09-09 17:22:03 +0900

    12123 1 2 3 4 5 6 7 8 9 10

---
+++ src/Java_UpGrade/Java_Bulder_Pattern1.java
@@ -0,0 +1,89 @@
+package Java_UpGrade;
+
+import javax.xml.soap.SOAPPart;
+
+public class Java_Bulder_Pattern1 {
+    public static void main(String[] args) {
+        Car1 car1=new Car1("V6", true, "red", false, true );
+
+        Car1 car2=new CarBuilder1()
+                .setEngine("V4")
+                .setColor("blue")
+                .build();
+
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+
+
+}
+
+
+class Car1{
+    private String engine;
+    private boolean airbag;
+    private String color;
+    private boolean carmeraSense;
+    private boolean ABS;
+
+    public Car1(String engine, boolean airbag, String color, boolean carmeraSense, boolean ABS) {
+        this.engine = engine;
+        this.airbag = airbag;
+        this.color = color;
+        this.carmeraSense = carmeraSense;
+        this.ABS = ABS;
+    }
+
+    @Override
+    public String toString() {  //car객체를 설명하기 위해서...
+        return "Car1{" +
+                "engine='" + engine + '\'' +
+                ", airbag=" + airbag +
+                ", color='" + color + '\'' +
+                ", carmeraSense=" + carmeraSense +
+                ", ABS=" + ABS +
+                '}';
+    }
+
+}
+
+class CarBuilder1{
+    private String engine;
+    private boolean airbag;
+    private String color;
+    private boolean carmeraSense;
+    private boolean ABS;
+
+    public CarBuilder1 setEngine(String engine) {  //set계열 메서드이지만 반환타입이 클래스 객체임을 주목
+        this.engine = engine;
+        return this;    //객체를 반환하여야 메서드 체이닝을 할 수 있다.
+        // 메서드 체이닝:carbuilder1.setEngine(gear).setColer(blue).setABS(true) 이런식으로 메서드들을 연결하는것
+    }
+
+    public CarBuilder1 setAirbag(boolean airbag) {
+        this.airbag = airbag;
+        return this;
+    }
+
+    public CarBuilder1 setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public CarBuilder1 setCarmeraSense(boolean carmeraSense) {
+        this.carmeraSense = carmeraSense;
+        return this;
+    }
+
+    public CarBuilder1 setABS(boolean ABS) {
+        this.ABS = ABS;
+        return this;
+    }
+
+    public Car1 build(){
+        return new Car1(engine, airbag, color, carmeraSense, ABS);
+        //CarBuilder1의 필드값을 Car1의 생성자에 주입했다.
+    }
+
+
+}
